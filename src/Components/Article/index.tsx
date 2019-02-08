a3ad43333d403a177549dcea6cb347c447b8beaf
nikomaniac <niko.sky.lan@gmail.com>
2019-02-08 20:02:50 +0800

    article base template

--- src/Components/Article/index.tsx
+++ src/Components/Article/index.tsx
@@ -2,12 +2,21 @@ import * as React from 'react';
 import IArticle from '../../Definition/IArticle';
 
 interface IOwnProps {
-  articles: IArticle[];
+  articleInfo: IArticle;
 }
 
 class Article extends React.Component<IOwnProps> {
   public render(): React.ReactNode {
-    return undefined;
+    const { articleInfo } = this.props;
+    return (
+      <section>
+        <h4>{articleInfo.title}</h4>
+        <div>基本信息</div>
+        <p>
+          {articleInfo.contentForHTML}
+        </p>
+      </section>
+    );
   }
 }
 
