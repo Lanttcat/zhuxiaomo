83d65fb3e8fe0f823517ff766df79265b6e3b396
NikoManiac <niko.sky.lan@gmail.com>
2019-04-02 22:28:36 +0800

    footer basic

---
+++ src/Components/Footer/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import classnames from 'classnames/bind';
+// @ts-ignore
+import style from './Footer.scss';
+const cx = classnames.bind(style);
+
+class Footer extends React.PureComponent{
+  render(): React.ReactNode {
+    return (
+      <footer className={cx('footer')}>
+        <div className="item item-1">Yixing</div>
+        <div className="item item-1">1</div>
+        <div className="item item-2">1</div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
